c1a17b6e589667d45e43c39240a2d0347fea7d43
The-greenEye <156267582+The-greenEye@users.noreply.github.com>
2024-05-02 04:54:45 +0300

    Update main.js

--- assest/scripts/main.js
+++ assest/scripts/main.js
@@ -3,17 +3,12 @@ window.onload = function () {
 }
 let NameUser = document.getElementById("username");
 let sexy = document.getElementById("nameUser");
-// let data = []
-// if (localStorage.user != null) {
-//     data = JSON.stringify(localStorage.user);
-// } else {
-//     data = []
-// }
 function sentData() {
   let dataName = {
     name: NameUser.value,
   };
   localStorage.setItem("user", JSON.stringify(dataName));
+    location.reload()
 }
 function getData() {
   const storedData = localStorage.getItem("user");
